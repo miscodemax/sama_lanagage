fdcb4900f067559de67fed410543c23e83b0f1cc
miscodemax <nmamadoundir@gmail.com>
2026-08-10 10:15:36 +0000

    commit combinaisons wwith dolli v0.2

--- interpreter.py
+++ interpreter.py
@@ -2,6 +2,37 @@ import sys
 from pathlib import Path
 import re
 
+def combinaisons(line, idx):
+
+    error_message = f'erreur de syntaxe a la ligne {idx} essaye "wax"'
+
+    if not line.startswith('wax'):
+        print(error_message)
+
+    line = line[4:].strip()
+
+    parts = re.split(r'\s+dolli\s+', line)
+    parts_copy = parts.copy()
+    
+
+    for i, part in enumerate(parts):
+        string_pattern = re.match(r'^"(.*)"$', part)
+        number_pattern = re.match(r'^(-?\d+(?:\.\d+)?)$', part)
+
+        if string_pattern:
+            parts_copy[i] = parts_copy[i].strip('"')
+
+
+        elif number_pattern:
+            parts[i] = float(parts[i])
+
+        else:
+            print(error_message)
+
+
+    return ('').join(parts_copy)
+    
+
 if len(sys.argv) == 1:
     print('aucun fichier trouvable')
 
@@ -19,6 +50,8 @@ else:
         for idx, line in enumerate(code):
 
             line = line.strip()
+            combi = 'dolli' in line
+            
 
             string_pattern = r'^wax\s+"(.*)"$'
             number_pattern = r'^wax\s+(-?\d+(?:\.\d+)?)$'
@@ -26,15 +59,18 @@ else:
             string_match = re.match(string_pattern, line)
             number_match = re.match(number_pattern, line)
 
-            if string_match:
+            if string_match and not combi:
 
                 message = string_match.group(1)
                 print(message)
 
-            elif number_match:
+            elif number_match and not combi:
 
                 message = number_match.group(1)
                 print(message)
+
+            elif combi:
+                print(combinaisons(line, idx))
 
             else:
                 print(f'erreur de syntaxe ligne {idx}')

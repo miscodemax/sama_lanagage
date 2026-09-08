ef65a6ba584f5ce950026f0124b0ae0dbfe596c6
miscodemax <nmamadoundir@gmail.com>
2026-09-08 20:32:50 +0000

    nouvelle feature

--- interpreter.py
+++ interpreter.py
@@ -2,7 +2,26 @@ import sys
 from pathlib import Path
 import re
 
-def combinaisons(line, idx):
+
+variables: dict[str, int] = {}
+
+def operations(a: int | float, operation: str, b: int | float) -> int | float | bool:
+
+    match operation:
+        case '+':
+            return a + b
+        case '-':
+            return a - b
+        case '*':
+            return a * b
+        case '/':
+            return a / b
+
+
+    
+    
+
+def combinaisons(line: list[str | int], idx: int) -> str:
 
     error_message = f'erreur de syntaxe a la ligne {idx} essaye "wax"'
 
@@ -16,8 +35,23 @@ def combinaisons(line, idx):
     
 
     for i, part in enumerate(parts):
-        string_pattern = re.match(r'^"(.*)"$', part)
-        number_pattern = re.match(r'^(-?\d+(?:\.\d+)?)$', part)
+
+        if part in variables:
+            parts[i] = str(variables[parts[i]])
+            parts_copy[i] = str(variables[parts_copy[i]])
+            
+                   
+
+        string_pattern = re.match(r'^"(.*)"$', parts[i])
+        number_pattern = re.match(r'^(-?\d+(?:\.\d+)?)$', parts[i])
+        pattern = r'^(-?\d+(?:\.\d+)?)\s*([+\-*/%])\s*(-?\d+(?:\.\d+)?)$'
+        var_op_pattern = r'^([a-zA-Z_]\w*)\s*([+\-*/%])\s*([a-zA-Z_]\w*)$'
+        var_op_match = re.match(var_op_pattern, parts[i])
+        operation_pattern = re.match(pattern, parts[i])
+
+
+       
+    
 
         if string_pattern:
             parts_copy[i] = parts_copy[i].strip('"')
@@ -26,6 +60,27 @@ def combinaisons(line, idx):
         elif number_pattern:
             parts[i] = float(parts[i])
 
+        elif operation_pattern:
+            a = float(operation_pattern.group(1))
+            operation = operation_pattern.group(2)
+            b = float(operation_pattern.group(3))
+            resultats = operations(a, operation, b)
+
+            if resultats == False:
+                print(f'Erreur a la ligne {idx} erreurr de frappe operation')
+
+            parts_copy[i] = str(resultats)
+
+        elif var_op_match:
+            first_var = variables[var_op_match.group(1)]
+            op = var_op_match.group(2)
+            second_var = variables[var_op_match.group(3)]
+            res = operations(first_var, op, second_var)
+
+            if res == False:
+                print(f'Erreur a la ligne {idx} erruer de frappe operation')
+            
+            parts_copy[i] = str(res)
         else:
             print(error_message)
 
@@ -33,11 +88,14 @@ def combinaisons(line, idx):
     return ('').join(parts_copy)
     
 
+
+
+
 if len(sys.argv) == 1:
     print('aucun fichier trouvable')
 
 else:
-    filename = Path(sys.argv[1])
+    filename: str = Path(sys.argv[1])
 
     if filename.suffix != '.wollof':
         print('erreur fichier wollof introuvable!')
@@ -51,10 +109,18 @@ else:
 
             line = line.strip()
             combi = 'dolli' in line
+
+            if len(line) == 0:
+                continue
             
 
             string_pattern = r'^wax\s+"(.*)"$'
             number_pattern = r'^wax\s+(-?\d+(?:\.\d+)?)$'
+            pattern = r'^wax\s+(-?\d+(?:\.\d+)?)\s*([+\-*/%])\s*(-?\d+(?:\.\d+)?)$'
+            operation_pattern = re.match(pattern, line)
+            variable_pattern = r'^([a-zA-Z_]\w*)\s+moy\s+(.+)$'
+            match_variable = re.match(variable_pattern, line)
+
 
             string_match = re.match(string_pattern, line)
             number_match = re.match(number_pattern, line)
@@ -72,5 +138,30 @@ else:
             elif combi:
                 print(combinaisons(line, idx))
 
+            elif operation_pattern:
+                a = float(operation_pattern.group(1))
+                operation = operation_pattern.group(2)
+                b = float(operation_pattern.group(3))
+                resultats = operations(a, operation, b)
+    
+                if resultats == False:
+                    print(f'Erreur a la ligne {idx} erreur de frappe operation')
+
+                print(resultats)
+
+            elif match_variable:
+                var_name = match_variable.group(1)
+                var_value = match_variable.group(2)
+
+                string_var_pattern = re.match(r'^"(.*)"$', var_value)
+                number_var_pattern = re.match(r'^(-?\d+(?:\.\d+)?)$', var_value)
+
+                if number_var_pattern:
+                    var_value = float(var_value)
+
+                variables[var_name] = var_value
+
             else:
                 print(f'erreur de syntaxe ligne {idx}')
+
+
